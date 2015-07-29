bbb0e54e8db62db9987ebb324c111e365f0b2572
Christopher Grant <super.saga@hotmail.com>
2015-07-29 16:49:44 -0400

    quit

--- OpenRA.Mods.D2k/Traits/Spicebloom.cs
+++ OpenRA.Mods.D2k/Traits/Spicebloom.cs
@@ -12,11 +12,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using OpenRA.Activities;
 using OpenRA.GameRules;
 using OpenRA.Graphics;
 using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
 using OpenRA.Support;
 using OpenRA.Traits;
+using OpenRA.Mods.Common.Activities;
 
 namespace OpenRA.Mods.D2k.Traits
 {
@@ -26,10 +29,10 @@ namespace OpenRA.Mods.D2k.Traits
 		public readonly string[] GrowthSequences = { "grow1", "grow2", "grow3" };
 
 		[Desc("The range of time (in ticks) that the spicebloom will take to respawn.")]
-		public readonly int[] RespawnTime = { 1500, 2500 };
+		public readonly int[] RespawnDelay = { 1500/50, 2500/50 };
 
 		[Desc("The range of time (in ticks) that the spicebloom will take to grow.")]
-		public readonly int[] GrowthTime = { 1000, 1500 };
+		public readonly int[] GrowthDelay = { 1000/50, 1500/50 };
 
 		public readonly string ResourceType = "Spice";
 
@@ -37,16 +40,13 @@ namespace OpenRA.Mods.D2k.Traits
 		[WeaponReference]
 		public readonly string Weapon = "SpiceExplosion";
 
-		[Desc("The amount of spice to expel.")]
-		public readonly int Pieces = 7;
+		[Desc("The range of spice to expel.")]
+		public readonly int[] Pieces = { 7, 9 };
 
-		[Desc("The maximum distance in cells that spice may be expelled.")]
-		public readonly int Range = 5;
+		[Desc("The range in cells that spice may be expelled.")]
+		public readonly int[] Range = { 5, 7 };
 
-		public object Create(ActorInitializer init)
-		{
-			return new Spicebloom(init, this);
-		}
+		public object Create(ActorInitializer init) { return new Spicebloom(init, this); }
 	}
 
 	public class Spicebloom : ITick, INotifyKilled
@@ -57,10 +57,11 @@ namespace OpenRA.Mods.D2k.Traits
 		readonly ResourceLayer resLayer;
 		readonly Health health;
 		readonly AnimationWithOffset anim;
+		readonly string race;
 
 		int counter;
-		int respawnTime;
-		int growTime;
+		int respawnTicks;
+		int growTicks;
 
 		public Spicebloom(ActorInitializer init, SpicebloomInfo info)
 		{
@@ -81,9 +82,11 @@ namespace OpenRA.Mods.D2k.Traits
 
 			render.Add(anim);
 
-			respawnTime = self.World.SharedRandom.Next(info.RespawnTime[0], info.RespawnTime[1]);
-			growTime = self.World.SharedRandom.Next(info.GrowthTime[0], info.GrowthTime[1]);
+			respawnTicks = self.World.SharedRandom.Next(info.RespawnDelay[0], info.RespawnDelay[1]);
+			growTicks = self.World.SharedRandom.Next(info.GrowthDelay[0], info.GrowthDelay[1]);
 			anim.Animation.Play(info.GrowthSequences[0]);
+			
+			Game.Debug("Hi, I'm spicebloom {0}", self.ActorID);
 		}
 
 		public void Tick(Actor self)
@@ -92,42 +95,45 @@ namespace OpenRA.Mods.D2k.Traits
 			{
 				counter++;
 
-				if (counter >= growTime)
+				if (counter >= growTicks)
 					self.Kill(self);
 				else
 				{
-					var index = info.GrowthSequences.Length * counter / growTime;
+					var index = info.GrowthSequences.Length * counter / growTicks;
 					anim.Animation.Play(info.GrowthSequences[index]);
-				}
-			}
-			else
-			{
-				counter++;
-
-				if (counter >= respawnTime)
-				{
-					counter = 0;
-					health.Resurrect(self, self);
-					respawnTime = self.World.SharedRandom.Next(info.RespawnTime[0], info.RespawnTime[1]);
-					growTime = self.World.SharedRandom.Next(info.GrowthTime[0], info.GrowthTime[1]);
-					anim.Animation.Play(info.GrowthSequences[0]);
 				}
 			}
 		}
 
 		public void Killed(Actor self, AttackInfo e)
 		{
-			counter = 0;
+			self.QueueActivity(new Wait(respawnTicks));
+			self.QueueActivity(new CallFunc(() =>
+				self.World.AddFrameEndTask(w =>
+				{
+					var a = w.CreateActor(self.Info.Name, new TypeDictionary
+					{
+						new LocationInit(self.Location),
+						new HealthInit(health.MaxHP),
+						new OwnerInit(self.Owner)
+					});
+					Game.Debug("{0} spawned {1}", self.ActorID, a.ActorID);
+					self.Dispose();
+				})
+			));
 
+			var pieces = self.World.SharedRandom.Next(info.Pieces[0], info.Pieces[1]);
 			var wep = self.World.Map.Rules.Weapons[info.Weapon.ToLowerInvariant()];
 
-			for (var i = 0; info.Pieces > i; i++)
+			for (var i = 0; pieces > i; i++)
 			{
+				var range = self.World.SharedRandom.Next(info.Pieces[0], info.Pieces[1]);
+
 				var cells = OpenRA.Traits.Util.RandomWalk(self.Location, self.World.SharedRandom);
-				var cell = cells.Take(info.Range).SkipWhile(p => resLayer.GetResource(p) == resType && resLayer.IsFull(p)).Cast<CPos?>().RandomOrDefault(self.World.SharedRandom);
+				var cell = cells.Take(range).SkipWhile(p => resLayer.GetResource(p) == resType && resLayer.IsFull(p)).Cast<CPos?>().RandomOrDefault(self.World.SharedRandom);
 
 				if (cell == null)
-					cell = cells.Take(info.Range).Random(self.World.SharedRandom);
+					cell = cells.Take(range).Random(self.World.SharedRandom);
 
 				var args = new ProjectileArgs
 				{
